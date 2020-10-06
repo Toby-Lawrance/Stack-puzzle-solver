bab776ce4955fe8700a102bafb3ccace514a53a7
Toby <toby.lawrance@gmail.com>
2020-10-06 02:48:59 +0100

    Fixed to working

--- Program.fs
+++ Program.fs
@@ -3,6 +3,14 @@
 open System
 open System.Collections.Immutable
 
+let log (value) (otherThing) =
+    printfn "%s" value
+    otherThing
+    
+let logValue (prefix) (value) =
+    printfn "%s%s" prefix (value.ToString())
+    value
+
 [<Literal>]
 let maxStackSize = 4
 
@@ -10,6 +18,7 @@ type Piece = Piece of char
 
 type Index = Index of int
 
+[<StructuredFormatDisplay("{AsString}");CustomEquality;NoComparison>]
 type Stack =
     { stack: Piece list
       index: Index }
@@ -18,7 +27,15 @@ type Stack =
         |> Array.ofList
         |> Array.map (fun (Piece c) -> c)
         |> String.Concat
-        |> sprintf "(%s)"
+    member this.AsString =
+        this.ToString()
+    override this.GetHashCode() =
+        this.stack.GetHashCode()
+        
+    override this.Equals(arg) =
+        match arg with
+        | :? Stack as s -> this.stack = s.stack
+        | _ -> false
 
 type State =
     | State of Stack list
@@ -26,7 +43,7 @@ type State =
         let (State s) = this
 
         s
-        |> List.map string
+        |> List.map (sprintf "(%A)")
         |> String.concat ","
         |> sprintf "[%s]"
 
@@ -38,7 +55,8 @@ type Place =
 
 type Node<'a> =
     { value: 'a
-      parent: Node<'a> option }
+      parent: Node<'a> option
+      depth: int }
     override this.ToString() =
         Some this
         |> List.unfold (Option.map (fun c -> (c.value,c.parent)))
@@ -112,8 +130,14 @@ let GenerateMoves (s: Node<State>) (queue : Node<State> list) (visited : Node<St
     |> List.filter allowedMove
     |> List.map makeMove
     |> List.map (makeState s.value)
-    |> List.map (fun state -> { value = state; parent = Some s }) //This is all our possible new moves
-    |> List.filter (fun move -> ( not(List.contains move.value (List.map (fun getState -> getState.value) queue)) && not(List.contains move.value (List.map (fun getState -> getState.value) visited))) )
+    |> List.map (fun state -> { value = state; parent = Some s; depth = (s.depth + 1) }) //This is all our possible new moves
+    |> List.distinctBy (fun n -> ( let (State ns) = n.value
+                                   List.sortBy string ns))
+    |> List.filter (fun move -> ( let (State ns) = move.value
+                                  let nsl = List.sortBy string ns
+                                  not(List.contains nsl (List.map (fun getState -> let (State gSl) = getState.value
+                                                                                   List.sortBy string gSl) (queue @ visited)))
+                                ) )
     |> List.append queue
 
 let CheckSolved (State s) =
@@ -125,13 +149,15 @@ let CheckSolved (State s) =
     List.forall CheckStack s
 
 let Solve (State s) =
-    let rec Solve' (pred) (queue) (visited) =
+    let rec Solve' (pred) (queue) (visited) (currentDepth) =
         match queue with
         | [] -> None
         | x :: _ when pred x.value -> Some x
-        | x :: xs -> Solve' pred (GenerateMoves x xs visited) (x::visited)
+        | x :: xs -> match x.depth with
+                     | d when d = currentDepth -> Solve' pred (GenerateMoves x xs visited) (x::visited) currentDepth
+                     | d when d <> currentDepth -> Solve' pred (GenerateMoves x xs visited) (x::visited) (log ("Depth increased to: " + d.ToString() + " QueueSize: " + (xs.Length).ToString()) d)
 
-    Solve' CheckSolved [ { value = State s; parent = None } ] []
+    Solve' CheckSolved [ { value = State s; parent = None; depth = 0 } ] [] -1
 
 [<EntryPoint>]
 let main argv =
